a1ba2a0c25d7cd074ead78855cdd6300bf67177e
gusanitor8 <gusanitor8@gmail.com>
2022-06-01 17:37:09 -0600

    Update app.js
    Se arreglo un bug y se documento parte del codigo

--- NeoTest/app.js
+++ NeoTest/app.js
@@ -17,21 +17,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456'));
 var session = driver.session();
-var session2 = driver.session();
 
 app.get('/', function(req, res){
     session
-        .run('MATCH (a: Person) RETURN a LIMIT 5')
+        .run('MATCH (a: Movie) RETURN a LIMIT 5')                   //Se corre la sentencia en Neo4J                       
         .then(function(result){
             var movieArr = [];
-            result.records.forEach(function(record){
+            result.records.forEach(function(record){                //Se ponen los resultados en un Array
                 movieArr.push({
-                    id: record._fields[0].identity.low,
-                    title: record._fields[0].properties.name,
-                    year: record._fields[0].properties.year
+                    id: record._fields[0].identity.low,                 //id de la serie
+                    title: record._fields[0].properties.title,          //Titulo de la serie
+                    year: record._fields[0].properties.year             //anio de la serie
                 })
             });
-            res.render('index', {
+            console.log(movieArr); //BORRAME                                     
+            res.render('index', {                                       //Se pasa movieArr a index.ejs
                 movies: movieArr
             })
         })
@@ -44,18 +44,23 @@ app.get('/', function(req, res){
 
 //<form method = "post" action = "/movie/add"> 
 app.post('/movie/add',function(req, res){
-    var title = req.body.movie_title;
-    var year = req.body.movie_year;
+    var title = req.body.movie_title;                   //movie_title hace referencia al nombre definido en el .ejs
+    var year = req.body.movie_year;                     //movie_year hace referencia al nombre definido en el .ejs 
 
-    session2 
-        .run('CREATE (n: Movie {title: "'+ title + '", year: "' + year + '"}) RETURN n.title')
-        .then(function(result){})
+    session 
+        .run('CREATE (n: Movie {title: "'+ title + '", year: "' + year + '"})')
+        .then(function(result){
+            res.redirect('/');
+            session.close();
+        })
         .catch(function(err){
            console.log(err); 
         });
-        res.redirect('/'); 
-        session2.close();  
+    res.redirect('/');                      //Regresa el 'buffer' al principio para poder llamar mas funciones en el .ejs
 });
+
+
+
 
 
 app.listen(3000);
